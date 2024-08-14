6a66e1c85925c95085880b3497468968d62fb545
Syed Suhaib Zia <suhaibzia786@gmail.com>
2024-08-14 11:29:05 +0400

    Text color corrected

--- resources/views/components/layouts/website-layout.blade.php
+++ resources/views/components/layouts/website-layout.blade.php
@@ -33,13 +33,7 @@
         <link rel="stylesheet" href="{{asset('assets/css/style.min.css')}}">
     -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/heroicons-css@0.1.1/heroicons.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        * {
-            color : #7d3800 !important;
-            font-family: 'Times New Roman', Times, serif;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
 </head>
 <body>
 
